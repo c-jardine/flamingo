0197e159beb5fc27223f863a16d568563f74f377
Chris Jardine <dev@keplux.com>
2022-10-23 22:25:29 -0400

    Add some initial tests.

---
+++ src/components/form/TextInput/TextInput.spec.tsx
@@ -0,0 +1,18 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import { render } from '@testing-library/react-native';
+import React from 'react';
+import TextInput from './TextInput';
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('../../../supabase/initSupabase.ts');
+
+describe('<TextInput />', () => {
+  it('should update the input text', async () => {
+    const { getByPlaceholderText } = render(
+      <TextInput placeholder='Test input' />
+    );
+    const textInput = getByPlaceholderText('Test input');
+    expect(textInput).not.toBeNull();
+    console.log(textInput.children)
+  });
+});
